af4ebe8b541de87f55e1f785a3955a638ebcda2e
Bùi Tiến Dũng <buitiendung12517@gmail.com>
2025-09-27 21:14:21 +0700

    Setup Prisma and User Model

--- server.js
+++ server.js
@@ -1,107 +1,14 @@
 import {ApolloServer,gql} from "apollo-server";
-import crypto from "crypto";
 
-const users = [
-  {
-    id: "d24259be-0f55-4b74-a859-1028a1b49b0a",
-    firstName: "mukesh",
-    lastName: "kumar",
-    email: "mukesh@mukeshkumar.com",
-    password: "123456"
-  },
-  {
-    id: "4cc1ec7c-76c7-4bdc-bf6a-a3b8ae643690",
-    firstName: "suresh",
-    lastName: "sharma",
-    email: "suresh@sureshsharma.com",
-    password: "123456"
-  },
-];
-
-const Todos = [
-    {
-        title: "buy book",
-        by: "d24259be-0f55-4b74-a859-1028a1b49b0a"
-    },
-    {
-        title: "write code",
-        by: "d24259be-0f55-4b74-a859-1028a1b49b0a"
-    },
-    {
-        title: "record video",
-        by: "4cc1ec7c-76c7-4bdc-bf6a-a3b8ae643690"
-    }
-]
+import typeDefs from "./typeDefs.js";
+import resolvers from "./resolvers.js";
 
 
-const typeDefs = gql`
-    type Query {
-        users: [User]
-        user(id: ID!): User
-    }
-
-    input UserInput {
-        firstName: String!
-        lastName: String!
-        email: String!
-        password: String!
-    }
-
-    type Mutation {
-        createUser(userNew: UserInput!): User
-    }
-
-    type User {
-        id: ID!
-        firstName: String!
-        lastName: String!
-        email: String!
-        todos: [Todo]
-    }
-
-    type Todo {
-        title: String
-        by: ID
-    }
-`;
-
-const resolvers = {
-    Query: {
-        users: () => users,
-        user: (parent, args, context) => {
-            if(!context.userLoggedIn) {
-                throw new Error("You are not logged in");
-            }
-            return users.find(item => item.id == args.id);
-        }
-    },
-    User: {
-        todos: (parent) => {
-            // console.log(parent);
-            return Todos.filter(todo => todo.by == parent.id);
-        }
-    },
-    Mutation: {
-        createUser: (_, {userNew}) => {
-            const record = {
-                id: crypto.randomUUID(),
-                ...userNew
-            };
-            users.push(record);
-
-            console.log(record);
-            return record;
-        }
-    }
-};
 
 const server = new ApolloServer(
     {
         typeDefs,
-        resolvers,
-        context: {
-            userLoggedIn: true
-        }
+        resolvers
     }
 );
 
