adae67e1351fedb9039d09f21c29925720503215
Bùi Tiến Dũng <buitiendung12517@gmail.com>
2025-09-27 17:46:48 +0700

    Tạo mới user

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import {ApolloServer,gql} from "apollo-server";
+import crypto from "crypto";
 
 const users = [
   {
-    id: 1,
+    id: "d24259be-0f55-4b74-a859-1028a1b49b0a",
     firstName: "mukesh",
     lastName: "kumar",
     email: "mukesh@mukeshkumar.com",
     password: "123456"
   },
   {
-    id: 2,
+    id: "4cc1ec7c-76c7-4bdc-bf6a-a3b8ae643690",
     firstName: "suresh",
     lastName: "sharma",
     email: "suresh@sureshsharma.com",
@@ -22,6 +23,17 @@ const typeDefs = gql`
     type Query {
         users: [User]
         user(id: ID!): User
+    }
+
+    input UserInput {
+        firstName: String!
+        lastName: String!
+        email: String!
+        password: String!
+    }
+
+    type Mutation {
+        createUser(userNew: UserInput!): User
     }
 
     type User {
@@ -38,6 +50,18 @@ const resolvers = {
         user: (parent, args, context) => {
             // console.log(args);
             return users.find(item => item.id == args.id);
+        }
+    },
+    Mutation: {
+        createUser: (_, {userNew}) => {
+            const record = {
+                id: crypto.randomUUID(),
+                ...userNew
+            };
+            users.push(record);
+
+            console.log(record);
+            return record;
         }
     }
 };
